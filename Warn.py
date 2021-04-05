8557bfa7ce16dc3fdab925f3d0cc441b9205b09a
Snowy1011 <80806310+Snowy1011@users.noreply.github.com>
2021-04-04 19:12:30 -0700

    Create Warn.py

---
+++ Warn.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands, tasks
+import os
+import json
+
+@client.command(pass_context = True)
+@has_permissions(manage_roles=True, ban_members=True)
+async def warn(ctx,user:discord.User,*reason:str):
+  if not reason:
+    await client.say("Please provide a reason")
+    return
+  reason = ' '.join(reason)
+  for current_user in report['users']:
+    if current_user['name'] == user.name:
+      current_user['reasons'].append(reason)
+      break
+  else:
+    report['users'].append({
+      'name':user.name,
+      'reasons': [reason,]
+    })
+  with open('reports.json','w+') as f:
+    json.dump(report,f)
