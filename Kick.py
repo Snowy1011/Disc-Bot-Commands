05dc9691900663d972d1a31c56acbed60fb8f439
Snowy1011 <80806310+Snowy1011@users.noreply.github.com>
2021-04-04 19:09:25 -0700

    Update Kick.py

--- Kick.py
+++ Kick.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands, tasks
 
+bot = commands.Bot(command_prefix="<Prefix>")
+
 @bot.command()
 async def kick(ctx, member : discord.Member, *, reason=None):
   await member.kick(reason=reason)
