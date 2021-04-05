92b20fbfc0daf30f195078d1b118fc6cc2c90237
Snowy1011 <80806310+Snowy1011@users.noreply.github.com>
2021-04-04 19:13:04 -0700

    Update Warn.py

--- Warn.py
+++ Warn.py
@@ -3,6 +3,8 @@ from discord.ext import commands, tasks
 import os
 import json
 
+bot = commands.Bot(command_prefix="<Prefix>")
+
 @client.command(pass_context = True)
 @has_permissions(manage_roles=True, ban_members=True)
 async def warn(ctx,user:discord.User,*reason:str):
